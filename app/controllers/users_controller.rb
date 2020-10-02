7a5b301c0baca88caa27d67eec906557ffd630e3
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-10-02 22:26:26 +0100

    Create User form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 class UsersController < ApplicationController
-    def new
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Great! Your user has been created!"
+      redirect_to new_user_path # go to show page for @user
+    else
+      flash.now[:error] = "Fix your mistakes, please."
+      render :new
     end
 
-    def create
-    end
+    # if @user.save
+    #   redirect_to new_user_path
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, allow_blank: false, uniqueness: true, length: { minimum: 5 }
+  validates :email, presence: true, allow_blank: false, uniqueness: true, confirmation: true
+  validates :password, presence: true, allow_blank: false, length: { minimum: 10 }
 end
