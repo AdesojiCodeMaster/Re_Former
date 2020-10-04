00c6b1b18a75c9f2bae2786b0785df8dc31a1e82
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-10-04 04:24:12 +0100

    Clear rubocop errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_174406) do # rubocop disable: Style/NumericLiterals
+ActiveRecord::Schema.define(version: 2020_10_02_174_406) do # rubocop disable: Style/NumericLiterals
   create_table 'users', force: :cascade do |t|
     t.string 'username'
     t.string 'email'
